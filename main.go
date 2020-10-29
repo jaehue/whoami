75a51435de4e041d4879c176984b33eb67a248c0
jaehue <bbugguj@gmail.com>
2020-10-29 23:51:37 +0800

    Call other url

--- main.go
+++ main.go
@@ -2,35 +2,32 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
-	"os"
 	"strings"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	message := fmt.Sprintln(r.Method, r.URL.Path)
-	fmt.Println(message)
-
-	if err := os.Mkdir("./logs", 0777); err != nil {
-		if !strings.Contains(err.Error(), "file exists") {
-			fmt.Println("[ERROR]Fail to create directory.", err)
-			fmt.Fprintf(w, "[%s]ERROR: %s", r.URL.Path[1:], err.Error())
-			return
-		}
+	if !strings.HasPrefix(r.URL.Path, "/call") {
+		fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+		return
 	}
-	f, err := os.OpenFile("./logs/info", os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
+	fmt.Println(r.URL.Path[6:])
+	resp, err := http.Get("http://" + r.URL.Path[6:])
 	if err != nil {
-		fmt.Println("[ERROR]Fail to open file.", err)
-		fmt.Fprintf(w, "[%s]ERROR: %s", r.URL.Path[1:], err.Error())
+		fmt.Fprintf(w, "%s[GET ERROR]%s", r.URL.Path[6:], err.Error())
 		return
 	}
 
-	if _, err := f.WriteString(message); err != nil {
-		fmt.Println("[ERROR]", err)
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Fprintf(w, "%s[READ ERROR]%s", r.URL.Path[6:], err.Error())
+		return
 	}
 
-	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+	fmt.Fprintf(w, "[URL]%s\n\n%s", r.URL.Path[6:], string(b))
+
 }
 
 func main() {
